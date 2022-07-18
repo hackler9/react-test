f2fe6425365da3e4906d8f99f40e6105ec47d142
hackler9 <56310539+hackler9@users.noreply.github.com>
2022-07-18 13:38:55 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+// src/App.js
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ]);
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
+      <AddTodo setTodos={setTodos}/>
+    </div>
+  );
+}
+
+function TodoList({ todos }) { //
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
